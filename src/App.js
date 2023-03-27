4a1b47cbe9ce9c978a4ff6c6cfc4d1c6eb275e15
Stefan Petković <93101094+cCoKeXx@users.noreply.github.com>
2023-03-27 14:09:59 +0200

    Meta data

--- src/App.js
+++ src/App.js
@@ -1,16 +1,14 @@
-import React, { Suspense } from "react";
+import React from "react";
 import Navbar from "./components/Navbar";
 import Homepage from "pages/Home";
-const Footer = React.lazy(() => import("./components/Footer"));
+import Footer from "./components/Footer";
+
 function App() {
   return (
     <div className="relative">
       <Navbar />
       <Homepage />
-      <Suspense fallback={<div>Loading...</div>}>
-        <Footer/>
-      </Suspense>
-
+      <Footer />
     </div>
   );
 }
